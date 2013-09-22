e6be84a2e31c24fbf23a0931474655b61a5083b9
Elhassan Rais <artistahub@gmail.com>
2013-09-22 01:39:19 -0700

    vides added to widget step 1

--- public/javascripts/domObjects.js
+++ public/javascripts/domObjects.js
@@ -58,8 +58,8 @@ var toolBarObjects = { myphotosToolBar : [{ name: "Photos", id : "myvideos-btn",
                      {name: "upload Videos", id : "myvideos-upload", link : "",class: "btn btn-success"}]};
 
 var widgetMenuIcons = function( userName ){
-       var iconItems = [{ name: 'myphotos', icon:'image-icon my-profile-icon', action: '/widget/' + userName, cssClass: 'widget-menu-icon '},
-           { name: 'myvideos', icon:'video-icon my-profile-icon', action: '/widget/' + userName + '/myvideos/', cssClass: 'widget-menu-icon'}];
+       var iconItems = [{ name: 'myphotos',id:'widget-myphotos-btn', icon:'image-icon my-profile-icon', action: '/widget/' + userName, cssClass: 'widget-menu-icon '},
+           { name: 'myvideos',id:'widget-myvideos-btn', icon:'video-icon my-profile-icon', action: '/widget/' + userName + '/myvideos/', cssClass: 'widget-menu-icon'}];
      return iconItems;
 }
 
